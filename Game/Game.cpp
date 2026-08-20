aff3999cb1ca9c0018760b6e33fbe0829e0a3c15
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2026-08-20 11:02:55 +0900

    ユニティちゃんのテクスチャの表示

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,20 +1,34 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "Game.h"
 
 
 bool Game::Start()
 {
 	// Load resources and set up your objects here (called once).
+	SpriteInitData initData;
+	initData.m_ddsFilePath[0] = "Assets/modelData/utc_all2.DDS";
+	initData.m_fxFilePath = "Assets/shader/sprite.fx";
+	initData.m_height = 256;
+	initData.m_width = 256;
+
+	m_sprite.Init(initData);
 	return true;
 }
 
 void Game::Update()
 {
 	// Per-frame logic goes here.
+	m_sprite.Update(
+		{ 0.0f,0.0f,0.0f },
+		Quaternion::Identity,
+		{ 1.0f,1.0f,1.0f }
+	);
+
 }
 
 void Game::Render(RenderContext& rc)
 {
 	// Your drawing code goes here.
 	// K2EngineLow already cleared the screen to gray before this is called.
+	m_sprite.Draw(rc);
 }
--- Game/Game.h
+++ Game/Game.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 // The starting point of your game.
 // This is a plain IGameObject running on K2EngineLow only.
@@ -14,4 +14,6 @@ public:
 	void Render(RenderContext& rc);
 
 private:
+
+	Sprite m_sprite;
 };
