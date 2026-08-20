9a2961abf3d6ca8a54ed9b95cdefd7aea9e8dd1f
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2026-08-20 11:47:52 +0900

    SpriteRenderの実装途中

--- k2EngineLow/graphics/SpriteRender.cpp
+++ k2EngineLow/graphics/SpriteRender.cpp
@@ -1,1 +1,27 @@
-﻿
+﻿#include "k2EngineLowPreCompile.h"
+
+#include "SpriteRender.h"
+
+namespace nsK2EngineLow
+{
+	void SpriteRender::Init(const char* ddsFilePath, const float width, const float height)
+	{
+		SpriteInitData initData;
+		initData.m_ddsFilePath[0] = ddsFilePath;
+		initData.m_fxFilePath = "Assets/shader/sprite.fx";
+		initData.m_width = static_cast<UINT>(width);
+		initData.m_height = static_cast<UINT>(height);
+
+		m_sprite.Init(initData);
+	}
+
+	void SpriteRender::Update()
+	{
+		m_sprite.Update(m_position, m_rotation, m_scale);
+	}
+
+	void SpriteRender::Draw(RenderContext& rc)
+	{
+		m_sprite.Draw(rc);
+	}
+}
--- k2EngineLow/graphics/SpriteRender.h
+++ k2EngineLow/graphics/SpriteRender.h
@@ -1,1 +1,39 @@
-﻿
+﻿#pragma once
+
+namespace nsK2EngineLow
+{
+	class SpriteRender
+	{
+	public:
+
+		void Init(const char* ddsFilePath, const float width, const float height);
+
+		void SetPosition(const Vector3& pos)
+		{
+			m_position = pos;
+		}
+
+		void SetRotation(const Quaternion& rot)
+		{
+			m_rotation = rot;
+		}
+
+		void SetScale(const Vector3& scale)
+		{
+			m_scale = scale;
+		}
+
+		void Update();
+
+		void Draw(RenderContext& rc);
+
+
+	private:
+
+		Sprite m_sprite;
+		Vector3 m_position;
+		Quaternion m_rotation;
+		Vector3 m_scale;
+
+	};
+}
